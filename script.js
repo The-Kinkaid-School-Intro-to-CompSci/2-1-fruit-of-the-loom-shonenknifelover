f78b501672b3a45f0ecad2cebcb7ef4a908fbdcd
sapphextwin <clericgrrl@gmail.com>
2024-04-09 14:48:10 -0500

    modified the script

--- script.js
+++ script.js
@@ -16,6 +16,27 @@ function clearCards() {
 function runProgram() {
     console.log("Program is running");
 
+    let apple = fruitObjects[0];
+    addFruitCard(apple);
+
+}
+
+function addFruitCard(fruit) {
+    let newFruitCard = document.createElement('div');
+
+    let fruitHeading = document.createElement('h3');
+    fruitHeading.textContent = apple.name;
+    newFruitCard.appendChild(fruitHeading);
+
+    let fruitColor = document.createElement('p');
+    fruitColor.textContent = `Color: ${apple.color}`;
+    fruitColor.style.color = apple.color;
+    newFruitCard.appendChild(fruitColor);
+
+
+    let fruitContainer = document.querySelector('#cards');
+    newFruitCard.classList.add('fruitCard');
+    fruitContainer.appendChild(newFruitCard);
 }
 
 document.addEventListener('DOMContentLoaded', runProgram);
